8ecda87863336b17dffb67399bc285b3cbde5542
faizaaulia <faizaaulia20@gmail.com>
2017-03-01 14:16:07 +0700

    modul 2 done!

--- asp_mvc_2/Controllers/AccountController.cs
+++ asp_mvc_2/Controllers/AccountController.cs
@@ -3,7 +3,7 @@ using System.Web.Security;
 using asp_mvc_2.Models.ViewModel;
 using asp_mvc_2.Models.EntityManager;
 
-namespace Aasp_mvc_2.Controllers
+namespace asp_mvc_2.Controllers
 {
     public class AccountController : Controller
     {
@@ -29,6 +29,43 @@ namespace Aasp_mvc_2.Controllers
                     ModelState.AddModelError("", "Login Name already taken.");
             }
             return View();
+        }
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(UserLoginView ULV, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                string password = UM.GetUserPassword(ULV.LoginName);
+
+                if (string.IsNullOrEmpty(password))
+                    ModelState.AddModelError("", "The user login or password provided is incorrect."); 
+                else {
+                    if (ULV.Password.Equals(password))
+                    {
+                        FormsAuthentication.SetAuthCookie(ULV.LoginName, false);
+                        return RedirectToAction("Welcome", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The password provided is incorrect."); 
+                    }
+                }
+            }
+
+            // If we got this far, something failed, redisplay form 
+            return View(ULV);
+        }
+        [Authorize]
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- asp_mvc_2/Controllers/HomeController.cs
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using asp_mvc_2.Security;
 
 namespace asp_mvc_2.Controllers
 {
@@ -11,6 +12,16 @@ namespace asp_mvc_2.Controllers
 
         [Authorize]
         public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+
+        public ActionResult UnAuthorized()
         {
             return View();
         }
